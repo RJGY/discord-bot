0bb112b8a398fc8359fd79c609eb654a3a6e7968
RJGY <rjgy2yu@gmail.com>
2021-10-08 19:48:59 +1100

    Changed request method

--- src/hate_commands/index.js
+++ src/hate_commands/index.js
@@ -1,9 +1,7 @@
 const who = require('./who');
 const request = require('./request');
-const coins = require('./coinManager');
 
 module.exports = {
     who,
     request,
-    coins,
 }
--- src/hate_commands/request.js
+++ src/hate_commands/request.js
@@ -1,11 +1,11 @@
-var getTitleAtUrl = require('get-title-at-url');
-/*
-(async(url) => {
-  var buf = await httpGet(url);
-  console.log(buf.toString('utf-8'));
-})('https://httpbin.org/headers');
-*/
-
+const getTitleAtUrl = require('get-title-at-url');
+const https = require('https')
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET'
+}
 
 module.exports = {
     name: 'request',
@@ -13,8 +13,29 @@ module.exports = {
     execute(message, args, bot) {
       message.channel.send(`Retrieving data...`);
       message.channel.send(`args: ${args}`);
+      const options = {
+        hostname: args.toString(),
+        port: 443,
+        path: '/todos',
+        method: 'GET'
+      }
+      const req = https.request(options, res => {
+        message.channel.send(`statusCode: ${res.statusCode}`)
+      
+        res.on('data', d => {
+          message.channel.send(`data: ${d}`);
+        })
+      })
+      
+      req.on('error', error => {
+        console.error(error)
+      })
+      
+      req.end()
+      /*
       getTitleAtUrl(args.toString(), function(title){
         message.channel.send(`title: ${title}`);
       });
+      */
     },
 }
