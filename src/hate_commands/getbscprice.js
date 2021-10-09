6de653004757a284e886f6fac816abe4195881f9
RJGY <rjgy2yu@gmail.com>
2021-10-09 20:21:42 +1100

    gets title of webpage now. added price check command

--- src/hate_commands/getbscprice.js
+++ src/hate_commands/getbscprice.js
@@ -1,15 +1,18 @@
 const puppeteer = require('puppeteer');
+const bsc_url = "https://charts.bogged.finance/?token="
+
+// Need to make it so that it gets the price after the ddos check.
 
 module.exports = {
-    name: 'request',
-    description: 'test request help?!',
+    name: 'getbscprice',
+    description: 'Get price of a binance smart chain coin.',
     execute(message, args, bot) {
       message.channel.send(`Retrieving data...`);
       message.channel.send(`args: ${args}`);
       (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto(args.toString());
+        await page.goto(bsc_url + args.toString());
         const title = await page.title();
         message.channel.send(`title: ${title}`);
         await browser.close();
--- src/hate_commands/index.js
+++ src/hate_commands/index.js
@@ -1,7 +1,9 @@
 const who = require('./who');
 const request = require('./request');
+const getbscprice = require('./getbscprice')
 
 module.exports = {
     who,
     request,
+    getbscprice,
 }
