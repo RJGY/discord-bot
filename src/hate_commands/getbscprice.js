bcca5f7ccb0bbad77fc24d20c50df1a1e880606b
RJGY <rjgy2yu@gmail.com>
2021-10-10 15:16:44 +1100

    Fixed proxy issue and added eth price check.

--- src/hate_commands/getbscprice.js
+++ src/hate_commands/getbscprice.js
@@ -12,7 +12,12 @@ module.exports = {
       (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
+        await page.setExtraHTTPHeaders({
+          'Accept-Language': 'en'
+        });
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36');
         await page.goto(bsc_url + args.toString());
+        await page.waitForNavigation();
         const title = await page.title();
         message.channel.send(`title: ${title}`);
         await browser.close();
--- src/hate_commands/index.js
+++ src/hate_commands/index.js
@@ -1,9 +1,11 @@
 const who = require('./who');
 const request = require('./request');
-const getbscprice = require('./getbscprice')
+const getbscprice = require('./getbscprice');
+const getethprice = require('./getethprice');
 
 module.exports = {
     who,
     request,
     getbscprice,
+    getethprice,
 }
